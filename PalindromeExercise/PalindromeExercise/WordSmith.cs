ff3b2738e77982e4f482b86c40585f6cf129ecdf
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-11 14:18:45 -0700

    Created the exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string palin)
+        {
+            if (string.IsNullOrEmpty(palin)) return false;
+            var charPalin = palin.ToCharArray();
+            Array.Reverse(charPalin);
+           var newPalin =  new string(charPalin);
+            return newPalin.ToLower().Replace(" ", "") == palin.ToLower().Replace(" ", "");
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("bob", true)]
+        [InlineData("deified", true)]
+        [InlineData("toot", true)]
+        [InlineData("Do Geese See God", true)]
+        [InlineData("dad", true)]
+        [InlineData("cat", false)]
+        [InlineData("don't do that", false)]
+        [InlineData("grateful", false)]
+        [InlineData("mad hatter", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void PalindromeTest(string palin, bool expected)
         {
+            var wordsmith = new WordSmith();
 
+            var actual = wordsmith.IsAPalindrome(palin);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
